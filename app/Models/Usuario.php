beca3f9e1b5ddbf32810503bd2bd42bba77e7242
CristianLopez08 <cristianlpz0812@gmail.com>
2025-06-18 23:42:24 -0600

    feat: agrego logica para cambio de uso de modelos de user como autenticador a modelo usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,33 +3,73 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens; // O use Laravel\Passport\HasApiTokens si usas Passport
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
+
     protected $table = 'TblUsuario';
     protected $primaryKey = 'IdUsuario';
     public $timestamps = false;
 
     protected $fillable = [
         'Nombre',
-        'Matricula', 
-        'Telefono', 
-        'Correo', 
+        'Matricula',
+        'Telefono',
+        'Correo',
         'Contrasena',
-        'IdCarrera', 
-        'IdRol', 
-        'FechaAlta', 
-        'FechaMod', 
+        'IdCarrera',
+        'IdRol',
+        'FechaAlta',
+        'FechaMod',
         'IdUsuarioMod'
     ];
+
+    protected $hidden = [
+        'Contrasena',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'FechaAlta' => 'datetime',
+        'FechaMod' => 'datetime',
+    ];
+
+    public function setContrasenaAttribute($value)
+    {
+        $this->attributes['Contrasena'] = Hash::make($value);
+    }
+
 
     public function carrera()
     {
         return $this->belongsTo(Carrera::class, 'IdCarrera');
     }
     
+
     public function rol()
     {
         return $this->belongsTo(Rol::class, 'IdRol');
+    }
+
+
+    public function getNombreCompletoAttribute()
+    {
+        return $this->Nombre;
+    }
+
+    public function scopePorMatricula($query, $matricula)
+    {
+        return $query->where('Matricula', $matricula);
+    }
+
+    
+    public function scopeActivos($query)
+    {
+        return $query->where('Estatus', 1);
     }
 }
